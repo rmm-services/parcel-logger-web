3a49c0752ab87069a9d6be40519e46f58cdc66f6
cheng <richellemaevillarin@gmail.com>
2023-08-15 22:54:06 +0800

    qr code update

---
+++ src/views/QrView.vue
@@ -0,0 +1,38 @@
+<script>
+export default{
+}
+</script>
+
+<template>
+  <div id="app">
+    <form>
+      <img src="../assets/submitted.png"/>
+      <h2>Please present this QR code to the concierge.</h2>
+      <vue-qrcode class="qr" :value="$route.params.qr" :options="{ width: 250 }"/>
+    </form>
+  </div>
+</template>
+
+<style>
+.qr{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+}
+
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 2rem;
+  height: 10%;
+  max-width: 10%;
+}
+
+h2{
+  color: black;
+  font-weight: bold;
+  font-family: "Lucida Console", "Courier New", monospace;
+}
+</style>
